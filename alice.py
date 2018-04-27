e59a941960b785261972051644c9bc4067875ee3
Haden Wasserbaech <spartanhaden@gmail.com>
2018-04-27 14:15:01 -0400

    Renamed bobs address and added exiting

--- alice.py
+++ alice.py
@@ -2,13 +2,14 @@
 
 import socket
 import binascii
+import threading
 from Crypto.Cipher import AES
 from Crypto.Random.random import getrandbits
 
 port = 8671
 kdc_port = 8888
 ip = '127.0.0.1'
-bob_server = (ip, port)
+bob_address = (ip, port)
 
 alices_key = b'23FCE5AE61E7BFCB29AC85725E7EC77DB9DBA460EACA7458070B719CE0B1DC31'
 
@@ -39,10 +40,10 @@ def decrypt(key, data):
     return cipher.decrypt_and_verify(binascii.unhexlify(ascii_ciphertext), binascii.unhexlify(ascii_digest))
 
 
-def setup_communication(sock, client_address):
+def setup_communication(sock, bob_address):
     # Send initial message to Bob
     print('Alice: Sending "Let\'s Talk" to Bob')
-    sock.sendto('Let\'s talk'.encode(), bob_server)
+    sock.sendto('Let\'s talk'.encode(), bob_address)
 
     # Receive message back from Bob
     print('Alice: Waiting for nonce from Bob')
@@ -72,7 +73,7 @@ def setup_communication(sock, client_address):
     # Generate second nonce for Alice and send message to Bob
     alices_nonce_2 = getrandbits(32)
     encrypted_nonce_2 = encrypt(session_key, str(alices_nonce_2).encode())
-    sock.sendto(ticket_for_bob + b' ' + encrypted_nonce_2, bob_server)
+    sock.sendto(ticket_for_bob + b' ' + encrypted_nonce_2, bob_address)
     print('Alice: Sent ticket to bob with new encrypted nonce of ' + str(alices_nonce_2))
 
     # Wait for the Alice's modified nonce and Bobs new nonce to come back from Bob
@@ -89,7 +90,7 @@ def setup_communication(sock, client_address):
 
     # Modify, encrypt and send Bob's nonce back
     message = encrypt(session_key, str(bobs_nonce_2 - 1).encode())
-    sock.sendto(message, bob_server)
+    sock.sendto(message, bob_address)
     print('Alice: Sending Bobs updated nonce of ' + str(bobs_nonce_2 - 1))
 
     # Authentication completed
@@ -109,13 +110,13 @@ if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(10)
 
-    session_key = setup_communication(sock, client_address)
     session_key = setup_communication(sock, bob_address)
 
     # Create thread to listen for incoming messages
-    threading.Thread(target=handle_receiving, args=(sock, session_key)).start()
+    receiver = threading.Thread(target=handle_receiving, args=(sock, session_key)).start()
 
     while True:
-        message = input('Send message').encode()
-        sock.sendto(name.encode() + b' ' + encrypt(session_key, message), client_address)
-
+        message = input().encode()
+        if message == b'exit':
+            exit()
+        sock.sendto(b'alice ' + encrypt(session_key, message), bob_address)
