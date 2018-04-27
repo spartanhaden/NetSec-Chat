4484da21c9581f5b52d95f18456a39dedf48d647
Haden Wasserbaech <spartanhaden@gmail.com>
2018-04-27 06:29:31 -0400

    Moved setup to seperate function

--- bob.py
+++ bob.py
@@ -2,6 +2,7 @@
 
 import socket
 import binascii
+import threading
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Random.random import getrandbits
@@ -53,6 +54,51 @@ def register_with_kdc(sock, name):
     sock.sendto(b'add_user ' + name.encode() + b' ' + binascii.hexlify(enc_user_key), (server_address, kdc_port))
 
 
+def setup_communication(sock, client_address):
+    bobs_nonce_1 = getrandbits(32)
+    print('Bob: Let\'s talk received, sending ' + str(bobs_nonce_1) + ' as the nonce')
+    message = encrypt(bobs_key, str(bobs_nonce_1).encode())
+    sock.sendto(message, client_address)
+
+    # Wait for Alice to get the session key from the KDC
+    payload, client_address = sock.recvfrom(4096)
+    print('Bob: Ticket and encrypted nonce received from Alice')
+
+    # Break apart the message from Alice
+    encrypted_ticket, alices_encrypted_nonce_2 = payload.split()
+    ticket = decrypt(bobs_key, encrypted_ticket)
+    split_ticket = ticket.split()
+    session_key = split_ticket[0]
+
+    # Verify the ticket
+    if split_ticket[1] != b'Alice' and decrypt(bobs_key, split_ticket[2]) != str(bobs_nonce_1).encode():
+        print('Bob: Ticket from Alice is invalid')
+        exit()
+
+    # Decrypt Alices second nonce and increment it
+    alices_nonce_2 = int(decrypt(session_key, alices_encrypted_nonce_2))
+    print('Bob: Decrypted Alice\'s nonce and found ' + str(alices_nonce_2))
+    alices_nonce_2 -= 1
+
+    # Create bob's second nonce, encrypts it with Alice's and sends it back
+    bobs_nonce_2 = getrandbits(32)
+    message = str(alices_nonce_2).encode() + b' ' + str(bobs_nonce_2).encode()
+    sock.sendto(encrypt(session_key, message), client_address)
+    print('Bob: Sent Bob\'s new nonce of ' + str(bobs_nonce_2) + ' and Alice\'s modified nonce of ' + str(alices_nonce_2))
+
+    # Wait for Bobs modified nonce to come back from Alice
+    encrypted_payload, client_address = sock.recvfrom(4096)
+    bobs_nonce_2_modified = int(decrypt(session_key, encrypted_payload).decode())
+    print('Bob: Bob\'s modified nonce ' + str(bobs_nonce_2_modified) + ' received from Alice')
+
+    # Verify the modified nonce from alice
+    if bobs_nonce_2 - 1 != bobs_nonce_2_modified:
+        print('Bob: Bob\'s nonce was not modified properly')
+        exit()
+
+    # Authentication completed
+    return session_key
+
 if __name__ == '__main__':
     # Setup socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -60,67 +106,15 @@ if __name__ == '__main__':
     sock.bind((server_address, server_port))
     print('Bob: Listening on ' + server_address + ':' + str(server_port))
 
+    # Waits for message from Alice
+    print()
+    print('Bob: waiting for request...')
+    payload, client_address = sock.recvfrom(4096)
+
+    # Receives first message from Alice and responds with an encrypted nonce
+    if payload.decode() == 'Let\'s talk':
+        session_key = setup_communication(sock, client_address)
+    else:
+        print('Bob: Improper message received, please restart')
+        exit()
     while True:
-        # Waits for message from Alice
-        print()
-        print('Bob: waiting for request...')
-        payload, client_address = sock.recvfrom(4096)
-
-        # Receives first message from Alice and responds with an encrypted nonce
-        if payload.decode() == 'Let\'s talk':
-            bobs_nonce_1 = getrandbits(32)
-            print('Bob: Let\'s talk received, sending ' + str(bobs_nonce_1) + ' as the nonce')
-            message = encrypt(bobs_key, str(bobs_nonce_1).encode())
-            sock.sendto(message, client_address)
-
-            # Wait for Alice to get the session key from the KDC
-            payload, client_address = sock.recvfrom(4096)
-            print('Bob: Ticket and encrypted nonce received from Alice')
-
-            # Break apart the message from Alice
-            encrypted_ticket, alices_encrypted_nonce_2 = payload.split()
-            ticket = decrypt(bobs_key, encrypted_ticket)
-            split_ticket = ticket.split()
-            session_key = split_ticket[0]
-
-            # Verify the ticket
-            if split_ticket[1] != b'Alice' and decrypt(bobs_key, split_ticket[2]) != str(bobs_nonce_1).encode():
-                print('Bob: Ticket from Alice is invalid')
-                continue
-
-            # Decrypt Alices second nonce and increment it
-            alices_nonce_2 = int(decrypt(session_key, alices_encrypted_nonce_2))
-            print('Bob: Decrypted Alice\'s nonce and found ' + str(alices_nonce_2))
-            alices_nonce_2 -= 1
-
-            # Create bob's second nonce, encrypts it with Alice's and sends it back
-            bobs_nonce_2 = getrandbits(32)
-            message = str(alices_nonce_2).encode() + b' ' + str(bobs_nonce_2).encode()
-            sock.sendto(encrypt(session_key, message), client_address)
-            print('Bob: Sent Bob\'s new nonce of ' + str(bobs_nonce_2) + ' and Alice\'s modified nonce of ' + str(alices_nonce_2))
-
-            # Wait for Bobs modified nonce to come back from Alice
-            encrypted_payload, client_address = sock.recvfrom(4096)
-            bobs_nonce_2_modified = int(decrypt(session_key, encrypted_payload).decode())
-            print('Bob: Bob\'s modified nonce ' + str(bobs_nonce_2_modified) + ' received from Alice')
-
-            # Verify the modified nonce from alice
-            if bobs_nonce_2 - 1 != bobs_nonce_2_modified:
-                print('Bob: Bob\'s nonce was not modified properly')
-                continue
-
-            # Wait for 'GET' from Alice
-            encrypted_payload, client_address = sock.recvfrom(4096)
-            payload = decrypt(session_key, encrypted_payload)
-            if payload != b'GET':
-                print('Bob: Message from Alice was not "GET" as expected')
-
-            # Send file back
-            in_file = open('ForAlice.txt', 'rb')
-            data = in_file.read()
-            in_file.close()
-            message = encrypt(session_key, data)
-            sock.sendto(message, client_address)
-            print('Bob: Sent the file to Alice')
-        else:
-            print('Bob: Improper message received, please restart')
